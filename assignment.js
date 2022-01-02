e2124fbbc927ea0fa0c08a4451c8585bae458d6a
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-01-02 16:14:04 -0500

    add work for initial first assignment

---
+++ assignment.js
@@ -0,0 +1,16 @@
+let userInput = 10;
+let result;
+
+result = 18 + userInput;
+
+result = result - 4;
+result = result * 2;
+result = result / 6;
+
+alert ('userInput = ' + userInput);
+alert ('result = ' + result);
+/* 
+The value of the user input did not change. It is unaffected and we did update the value of that variable.  
+ Only the value of the result would have changed and because that is what we are manipulating 
+ */
+
